36c8fbcad3ccf502667b5be0842329003bed2260
Michael <46394878+michaelthephoenix@users.noreply.github.com>
2023-11-07 13:44:06 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,100 +1,74 @@
 // create web server with express
 const express = require('express');
 const app = express();
-// create a port
 const port = 3000;
-// create a path
-const path = require('path');
-// create a body parser
+
+// import body-parser
 const bodyParser = require('body-parser');
-// create a mongoose
-const mongoose = require('mongoose');
-// create a method override
-const methodOverride = require('method-override');
-// create a express-sanitizer
-const expressSanitizer = require('express-sanitizer');
+app.use(bodyParser.urlencoded({extended: true}));
 
-// connect to mongoDB
-mongoose.connect('mongodb://localhost:27017/restful_blog_app', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-// set the view engine
-app.set('view engine', 'ejs');
-// set the path
-app.set('views', path.join(__dirname, 'views'));
-// set the body parser
-app.use(bodyParser.urlencoded({ extended: true }));
-// set the express sanitizer
-app.use(expressSanitizer());
-// set the method override
-app.use(methodOverride('_method'));
-// set the path for public
-app.use(express.static(path.join(__dirname, 'public')));
-
-// create a schema
-const blogSchema = new mongoose.Schema({
-  title: String,
-  image: String,
-  body: String,
-  created: { type: Date, default: Date.now },
-});
-// create a model
-const Blog = mongoose.model('Blog', blogSchema);
-
-// RESTful Routes
-// Blog.create({
-//   title: 'Test Blog',
-//   image:
-//     'https://images.unsplash.com/photo-1562967914-527ecf2aeeb0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-//   body: 'Hello this is a blog post',
-// });
-
-// RESTful Routes
-// Index Route
-app.get('/', (req, res) => {
-  res.redirect('/blogs');
+// import mysql
+const mysql = require('mysql');
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'blog'
 });
 
-// Index Route
-app.get('/blogs', (req, res) => {
-  Blog.find({}, (err, blogs) => {
-    if (err) {
-      console.log('ERROR!');
-    } else {
-      res.render('index', { blogs: blogs });
-    }
-  });
+// connect to mysql
+con.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to database');
 });
 
-// New Route
-app.get('/blogs/new', (req, res) => {
-  res.render('new');
+// get all comments
+app.get('/comments', (req, res) => {
+    let sql = 'SELECT * FROM comments';
+    let query = con.query(sql, (err, results) => {
+        if (err) throw err;
+        res.send(results);
+    });
 });
 
-// Create Route
-app.post('/blogs', (req, res) => {
-  // create blog
-  req.body.blog.body = req.sanitize(req.body.blog.body);
-  Blog.create(req.body.blog, (err, newBlog) => {
-    if (err) {
-      console.log('ERROR!');
-    } else {
-      // redirect to the index
-      res.redirect('/blogs');
-    }
-  });
+// get a comment by id
+app.get('/comments/:id', (req, res) => {
+    let sql = `SELECT * FROM comments WHERE id = ${req.params.id}`;
+    let query = con.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
 });
 
-// Show Route
-app.get('/blogs/:id', (req, res) => {
-Blog.findById(req.params.id, (err, foundBlog) => {
-    if (err) {
-        res.redirect('/blogs');
-    } else {
-        res.render('show', { blog: foundBlog });
-    }
+// add a new comment
+app.post('/comments', (req, res) => {
+    let data = {name: req.body.name, comment: req.body.comment};
+    let sql = 'INSERT INTO comments SET ?';
+    let query = con.query(sql, data, (err, results) => {
+        if (err) throw err;
+        res.send(results);
+    });
 });
 
+// update a comment
+app.put('/comments/:id', (req, res) => {
+    let sql = `UPDATE comments SET name = '${req.body.name}', comment = '${req.body.comment}' WHERE id = ${req.params.id}`;
+    let query = con.query(sql, (err, results) => {
+        if (err) throw err;
+        res.send(results);
+    });
+});
+
+// delete a comment
+app.delete('/comments/:id', (req, res) => {
+    let sql = `DELETE FROM comments WHERE id = ${req.params.id}`;
+    let query = con.query(sql, (err, results) => {
+        if (err) throw err;
+        res.send(results);
+    });
+});
+
+// listen on port 3000
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
